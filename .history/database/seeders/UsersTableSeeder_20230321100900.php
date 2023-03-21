123d2a551d8efc8b9daa411d56a433fd8a4c7d1e
James Harold II <JHarold1241@outlook.com>
2023-03-21 11:19:10 -0500

    Initial structure for uniquely constructed laravel application

---
+++ .history/database/seeders/UsersTableSeeder_20230321100900.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => "James Harold II",
+            "email" => "JHarold1241@outlook.com",
+            ""
+        ]);
+    }
+}
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => env('ADMIN_NAME'),
+            "email" => env('ADMIN_EMAIL'),
+            "password" => bcrypt(env('ADMIN_PASSWORD')),
+        ]);
+    }
+}
